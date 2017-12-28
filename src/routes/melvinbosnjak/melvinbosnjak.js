46253ab88bb926629f6d61d777488739e323f763
Raj Patel <rajpatel0820@gmail.com>
2017-12-27 22:08:03 -0700

    added melvins folder

--- src/routes/melvinbosnjak/melvinbosnjak.js
+++ src/routes/melvinbosnjak/melvinbosnjak.js
@@ -8,7 +8,7 @@ var router = express.Router();
 
 //api specific routes
 router.get('/', function(req, res) {
-  res.sendFile(__dirname + '/src/index/index.html');
+  res.send('hello world');
 });
 
 module.exports = router;
