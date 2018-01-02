0db6a38ae3b92fa909f9f1c1652e4bdf62d3d1a2
Raj Patel <rajpatel0820@gmail.com>
2018-01-02 00:40:15 -0700

    added shit

--- src/routes/rajpatel/src/index/index.js
+++ src/routes/rajpatel/src/index/index.js
@@ -1,9 +1,29 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+window.onload = function () {
+  var open = false;
+  document.getElementById('icon-label').onclick = function () {
+    document.getElementById('nav-bar-icon').style.transitionDuration = "250ms";
+    if (open) {
+      document.getElementById('nav-bar-icon').style.transform = "rotate(0deg)";
+    } else {
+      document.getElementById('nav-bar-icon').style.transform = "rotate(180deg)";
+    }
+    open = !open;
+  };
+};
 
+$(document).ready(function(){
+  var opennav = false;
+
+  $("#icon-label").click(function(){
+    if (opennav) {
+      $("ul").slideUp();
+    } else {
+      $("ul").slideDown();
+    }
+    opennav = !opennav;
+  });
+
+  $("#top-nav-bar li").each(function(i) {
+    $(this).delay(100 * i).slideDown(500);
+  });
+});
