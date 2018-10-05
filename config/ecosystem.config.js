08ecce9cb6848d7a56c2934a73386f23fbe8baf5
Raj Patel <rajpatel0820@gmail.com>
2018-10-04 22:04:36 -0600

    fix

--- config/ecosystem.config.js
+++ config/ecosystem.config.js
@@ -31,14 +31,6 @@ production.ssh_options = "StrictHostKeyChecking=no"
 production["pre-setup"] = "sudo npm i -g pm2"
 production["pre-deploy-local"] = "echo 'Local command'"
 production["post-deploy"] = "npm i && pm2 startOrRestart config/ecosystem.config.js --env production"
-production.env = {}
-production.env.NODE_ENV = "production"
-production.env.NODE_PATH = "."
-
-
-
 ecosystem.deploy.production = production
-
-
 
 module.exports = ecosystem
