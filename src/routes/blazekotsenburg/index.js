7e120bbc8fdcd3aced7952b0a79b45d71972c845
blazekotsenburg <u0676062@utah.edu>
2018-10-21 21:17:49 -0600

    Added routes and html files for all tabs in navbar to blazekotsenburg sub-domain.

--- src/routes/blazekotsenburg/index.js
+++ src/routes/blazekotsenburg/index.js
@@ -12,5 +12,29 @@ router.get('/', function(req, res) {
     res.sendFile(__dirname + '/web/index/index.html')
 })
 
+router.get('/about_me', function(req, res) {
+    res.sendFile(__dirname + '/web/about_me.html')
+})
+
+router.get('/resume', function(req, res) {
+    res.sendFile(__dirname + '/web/resume.html')
+})
+
+router.get('/experience', function(req, res) {
+    res.sendFile(__dirname + '/web/experience.html')
+})
+
+router.get('/skills', function(req, res) {
+    res.sendFile(__dirname + '/web/skills.html')
+})
+
+router.get('/coursework', function(req, res) {
+    res.sendFile(__dirname + '/web/coursework.html')
+})
+
+router.get('/projects', function(req, res) {
+    res.sendFile(__dirname + '/web/projects.html')
+})
+
 module.exports = router
 
